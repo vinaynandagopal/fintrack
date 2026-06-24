3c79ff8d2cb1e747798a851dddb56781608c8221
vinaynandagopal <vinaynanda06@gmail.com>
2026-06-24 12:28:34 +0530

    Add Docker setup

--- backend/run.py
+++ backend/run.py
@@ -1,6 +1,11 @@
+import os
 from app import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+    host = os.getenv("FLASK_RUN_HOST", "127.0.0.1")
+    port = int(os.getenv("FLASK_RUN_PORT", "5000"))
+    debug = os.getenv("FLASK_DEBUG", "True").lower() == "true"
+
+    app.run(host=host, port=port, debug=debug)
